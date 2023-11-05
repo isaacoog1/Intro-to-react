61343d212982457ac7fc4ea5d5eddaa32ab253e9
isaacoog1 <oguntuyokoredeshile@gmail.com>
2023-11-05 13:45:35 +0100

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import ListGroup from "./components/ListGroup";
+
+function App() {
+  let items = [
+    "New York",
+    "California",
+    "Washington",
+    "Las Vegas",
+    "Ohio",
+    "Chicago",
+  ];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
+}
+
+export default App;
